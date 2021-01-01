cd390ed5191492219874208e0758362e1fc3cfa0
@tyler-lady <tclady53@gmail.com>
2021-01-01 11:54:25 -0500

    initial and final commit

---
+++ Assets/Flock.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlockManager myManager; //link to our manager
+    float speed;
+    bool turning = false; //become true when swim limits reached
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //bounding box of the manager cube //Bounds is an invisible rectangular prism, created around a point, and at a certain size
+        Bounds b = new Bounds(myManager.transform.position, myManager.swimLimits * 2);
+
+        //if fish leaves bounding box or is about to collide w/ object, turn it around
+        RaycastHit hit = new RaycastHit(); //we create a raycast hit
+        Vector3 direction = Vector3.zero;
+        if (!b.Contains(transform.position))
+        {
+            turning = true;
+            direction = myManager.transform.position - transform.position;
+        }
+        else if (Physics.Raycast(transform.position, this.transform.forward * 50, out hit))
+        {
+            turning = true;
+            //Debug.DrawRay(this.transform.position, this.transform.forward * 50, Color.red); //draws the projected ray to show us correct collision prediction
+            direction = Vector3.Reflect(this.transform.forward, hit.normal);
+        }
+        else
+        {
+            turning = false;
+        }
+        if (turning)
+        {
+            //turn towards center of bounding box
+            
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), myManager.rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            //can use conditionals here with random % to make things more spontaneous
+            if (Random.Range(0, 100) < 10)
+            {
+                speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+            }
+            if (Random.Range(0, 100) < 20) //could also use an invoke function here
+            {
+                ApplyRules();
+            }
+        }
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+
+    void ApplyRules()
+    {
+        GameObject[] gos; //array for all of our current flock's fish
+        gos = myManager.allFish; //here we fill the array from myManager
+
+        Vector3 vcentre = Vector3.zero; //we initialize our vectors as zeroes
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.01f; //global speed of the group
+        float nDistance; //neighbour distance
+        int groupSize = 0; //initialized group size
+
+        foreach (GameObject go in gos) //loop thru all of the flock's fish
+        {
+            if (go != this.gameObject) //as long as the fish isn't our fish (the one running the script) do this 
+            {
+                nDistance = Vector3.Distance(go.transform.position, this.transform.position); //distance from the loop fish to our current fish
+                if (nDistance <= myManager.neighbourDistance) //if that distance is inside of our desired neighbour distancce
+                {
+                    vcentre += go.transform.position; //adds fish position to the vcentre vector
+                    groupSize++; //fish is added to group size
+
+                    if (nDistance < 1.0f) //distance between fish (my bubble)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position); //adds to the current avoid vector the position of the loop fish
+                    }
+
+                    Flock anotherFlock = go.GetComponent<Flock>(); //grabbing the other fishes code to pull speed
+                    gSpeed = gSpeed + anotherFlock.speed; //using the fish's speed to add to the global
+                }
+            }
+        }
+
+        if (groupSize > 0)
+        {
+            vcentre = vcentre / groupSize + (myManager.goalPos - this.transform.position); //here we added a goal position vector to the center of the group vector to create a goal for the overall movement of teh group
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vcentre + vavoid) - transform.position; //finding the direction our fish needs to go
+            if (direction != Vector3.zero) //if desired direction is not 0
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), myManager.rotationSpeed * Time.deltaTime); //perform this rotation to reach desired direction
+            }
+        }
+    }
+}
---
+++ Assets/FlockManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public GameObject fishPrefab; //will hold our fish prefab for instantiation
+    public int numFish = 50; //number of fish to be created
+    public GameObject[] allFish; //array that will hold our created fish
+    public Vector3 swimLimits = new Vector3(5, 5, 5); //meant to be a box around our flock manager where they can swim, can function as a container for fish
+    public Vector3 goalPos;
+
+    [Header("Fish Settings")] //this gives us header in the inspector displaying the string (very cool)
+    [Range(0.0f, 5.0f)] //this gives us a slider for the below public float in the inspector
+    public float minSpeed; //is assigning a variable connected to the range above thru the inspector
+    [Range(0.0f, 5.0f)] //we can set values of 0-5 in the inspector
+    public float maxSpeed; //is assigning a variable connected to the range above thru the inspector
+    [Range(0.0f, 20.0f)]
+    public float neighbourDistance;
+    [Range(0.0f, 5.0f)]
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allFish = new GameObject[numFish]; //creates our array will all the fish
+        for (int i = 0; i < numFish; i++) {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                                                                Random.Range(-swimLimits.y, swimLimits.y),
+                                                                Random.Range(-swimLimits.z, swimLimits.z));
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
+            allFish[i].GetComponent<Flock>().myManager = this; //links our two files together
+        }
+        // for each fish, create a position for them inside our swimLimits, then instantiate the fish using our fish prefab
+        goalPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 100) < 10) //creates a conditional based on a 10% chance. Very light work needed to check that every fram
+        {
+            goalPos = this.transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                                                                Random.Range(-swimLimits.y, swimLimits.y),
+                                                                Random.Range(-swimLimits.z, swimLimits.z));
+        }
+    }
+}
